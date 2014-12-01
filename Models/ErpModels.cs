1d655e7946a71d98c3da041059537981dbc3e161
Jakub Szumiato <szumiato@hotmail.com>
2014-12-01 21:58:18 +0100

    order models

--- Models/ErpModels.cs
+++ Models/ErpModels.cs
@@ -18,5 +18,36 @@ namespace PortalERP.Models
             public string Phone { get; set; }
             public string Address { get; set; }
         }
+        
+        //Orders
+        public class Item
+        {
+            public int id { get; set; }
+            public string code { get; set; }
+            public string name { get; set; }
+            public double price { get; set; }
+            public int quantity { get; set; }
+        }
+
+        public class Order
+        {
+            public string _id { get; set; }
+            public int index { get; set; }
+            public string guid { get; set; }
+            public bool isActive { get; set; }
+            public string value { get; set; }
+            public string numerator { get; set; }
+            public string referenceNumber { get; set; }
+            public string customer { get; set; }
+            public string email { get; set; }
+            public string phone { get; set; }
+            public string address { get; set; }
+            public string description { get; set; }
+            public string registered { get; set; }
+            public double latitude { get; set; }
+            public double longitude { get; set; }
+            public IList<string> tags { get; set; }
+            public IList<Item> items { get; set; }
+        }
     }
 }
