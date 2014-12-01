b9340c920cdd2270032b0e32fe8fc67b8198de2f
Jakub Szumiato <szumiato@hotmail.com>
2014-12-01 21:46:04 +0100

    model classes

---
+++ Models/ErpModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortalERP.Models
+{
+    public class ErpModels
+    {
+        public class Customer
+        {
+            public string Id { get; set; }
+            public int Index { get; set; }
+            public bool IsActive { get; set; }
+            public int Age { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string Phone { get; set; }
+            public string Address { get; set; }
+        }
+    }
+}
