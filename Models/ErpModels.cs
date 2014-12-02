3aafbb1a4a8c1e5d1edd2ffca2394d1d86a74d0d
Jakub Szumiato <szumiato@hotmail.com>
2014-12-02 21:07:50 +0100

    project changes

--- Models/ErpModels.cs
+++ Models/ErpModels.cs
@@ -5,24 +5,12 @@ using System.Runtime.InteropServices;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.Owin.BuilderProperties;
+using PortalERP.Models.Customers;
 
 namespace PortalERP.Models
 {
-
-    public class Customer
-    {
-        public string Id { get; set; }
-        public int Index { get; set; }
-        public bool IsActive { get; set; }
-        public int Age { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public string Address { get; set; }
-    }
-
     //Orders
-    public class Item
+    public class Element
     {
         public int id { get; set; }
         public string code { get; set; }
@@ -42,31 +30,7 @@ namespace PortalERP.Models
         public string Description { get; set; }
         public DateTime Registered { get; set; }
         public IList<string> Tags { get; set; }
-        public IList<Item> Items { get; set; }
-    }
-
-
-
-
-    public class Invoice
-    {
-        public string _id { get; set; }
-        public int index { get; set; }
-        public string guid { get; set; }
-        public bool isActive { get; set; }
-        public string value { get; set; }
-        public string numerator { get; set; }
-        public string referenceNumber { get; set; }
-        public string customer { get; set; }
-        public string email { get; set; }
-        public string phone { get; set; }
-        public string address { get; set; }
-        public string description { get; set; }
-        public string registered { get; set; }
-        public double latitude { get; set; }
-        public double longitude { get; set; }
-        public IList<string> tags { get; set; }
-        public IList<Item> items { get; set; }
+        public IList<Element> Items { get; set; }
     }
 
     public class Order : Document
@@ -81,7 +45,6 @@ namespace PortalERP.Models
         public double Longitude { get; set; }
         public City City { get; set; }
     }
-
 
     public class City
     {
