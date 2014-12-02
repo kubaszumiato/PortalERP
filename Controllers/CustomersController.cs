893684908f607233f5a6811fe7f24415e25af10d
Youenn-Bouglouan <youenn.bouglouan@gmail.com>
2014-12-02 23:08:17 +0100

    added cross support

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Results;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using PortalERP.Models;
